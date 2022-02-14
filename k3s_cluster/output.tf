6bf4d3254fe3181fe78a618765374fdca6dc772e
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2022-02-14 15:53:09 +0100

    Added output: elb_dns_name, k3s_server_public_ips and k3s_workers_public_ips

---
+++ k3s_cluster/output.tf
@@ -0,0 +1,12 @@
+output "elb_dns_name" {
+  value       = var.create_extlb ? aws_lb.external-lb.*.dns_name : []
+  description = "ELB public DNS name"
+}
+
+output "k3s_server_public_ips" {
+  value = data.aws_instances.k3s_servers.*.public_ips
+}
+
+output "k3s_workers_public_ips" {
+  value = data.aws_instances.k3s_workers.*.public_ips
+}
