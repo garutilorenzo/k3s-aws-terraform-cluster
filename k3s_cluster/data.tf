b37aec295acef0a3e82dd1ba13d03b35785ecd26
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2022-02-14 16:05:48 +0100

    Added some sources

--- k3s_cluster/data.tf
+++ k3s_cluster/data.tf
@@ -1,3 +1,37 @@
+data "aws_iam_policy" "AmazonEC2ReadOnlyAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+data "aws_instances" "k3s_servers" {
+  
+  depends_on = [
+    aws_autoscaling_group.k3s_servers_asg,
+  ]
+
+  instance_tags = {
+    k3s-instance-type = "k3s-server"
+    provisioner       = "terraform"
+    environment       = var.environment
+  }
+
+  instance_state_names = ["running"]
+}
+
+data "aws_instances" "k3s_workers" {
+  
+  depends_on = [
+    aws_autoscaling_group.k3s_workers_asg,
+  ]
+
+  instance_tags = {
+    k3s-instance-type = "k3s-worker"
+    provisioner       = "terraform"
+    environment       = var.environment
+  }
+
+  instance_state_names = ["running"]
+}
+
 data "template_cloudinit_config" "k3s_server" {
   gzip          = true
   base64_encode = true
