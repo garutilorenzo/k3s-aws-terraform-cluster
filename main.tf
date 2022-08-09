73d4f90f13b18c0897c40791af7358c81733dd35
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2022-08-09 10:41:07 +0200

    Added output in main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,15 @@ module "k3s_cluster" {
   k3s_token           = "<change_me>"
   source              = "./k3s_cluster/"
 }
+
+output "elb_dns_name" {
+  value = module.k3s_cluster.elb_dns_name
+}
+
+output "k3s_server_public_ips" {
+  value = module.k3s_cluster.k3s_server_public_ips
+}
+
+output "k3s_workers_public_ips" {
+  value = module.k3s_cluster.k3s_workers_public_ips
+}
