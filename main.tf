e581e83aa93bf70b9ac7d1bac4bbc0be250f53f3
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2022-10-14 14:30:06 +0200

    Updated main.tf example

--- main.tf
+++ main.tf
@@ -20,9 +20,11 @@ module "k3s_cluster" {
   vpc_subnets               = ["<change_me>", "<change_me>", "<change_me>"]
   vpc_subnet_cidr           = "<change_me>"
   cluster_name              = "k3s-cluster"
-  create_extlb              = true
+  create_extlb              = "<true|false>"
+  expose_kubeapi            = "<true|false>"
+  efs_persistent_storage    = "<true|false>"
   certmanager_email_address = "<change_me>"
-  source                    = "./k3s_cluster/"
+  source                    = "github.com/garutilorenzo/k3s-aws-terraform-cluster"
 }
 
 output "elb_dns_name" {
